b42a3212027d27f0aeeaf3a0b94b505761c5fc5f
Ben Smith <bens6477@gmail.com>
2021-12-06 12:35:07 +0000

    refactor: create_Board() function into Board class

--- run.py
+++ run.py
@@ -13,23 +13,25 @@ class Board:
         self.board_size = board_size
         self.num_ships = num_ships
         self.name = name
+        self.board = self.create_board()
+
+    
+    def create_board(self):
+        """
+        Creates a board for the player passed into the function.
+        """
+        print(self.name)
+        self.board = []
+        for row in range(self.board_size):
+            self.board.append([]) 
+            for column in range(self.board_size):
+                self.board[row].append("-")
+        return self.board
+
+user = Board(board_size, num_ships, user_name)
+computer = Board(board_size, num_ships, "computer")
 
 
-user_board = Board(board_size, num_ships, user_name)
-computer_board = Board(board_size, num_ships, "computer")
+print(user.board)
 
 
-def create_board(player):
-    """
-    Creates a board for the player passed into the function.
-    """
-    print(player.name)
-    board = []
-    for row in range(board_size):
-        board.append([]) 
-        for column in range(board_size):
-            board[row].append("-")
-    print(board)
-
-
-create_board(user_board)
