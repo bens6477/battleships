1746345d447b95c9cd19f0af2b267420fbdd113a
Ben Smith <bens6477@gmail.com>
2021-12-06 13:00:19 +0000

    feat: Add display_board() function to Board class to display player board in a clean and formatted structure

--- run.py
+++ run.py
@@ -1,5 +1,6 @@
+from pprint import pprint
+
 scores = {"computer": 0, "user": 0}
-print(scores)
 
 board_size = 5
 num_ships = 5
@@ -20,18 +21,30 @@ class Board:
         """
         Creates a board for the player passed into the function.
         """
-        print(self.name)
         self.board = []
+        counter = 0
         for row in range(self.board_size):
-            self.board.append([]) 
+            self.board.append([])
             for column in range(self.board_size):
-                self.board[row].append("-")
+                self.board[row].append(counter)
+                counter += 1
+        
         return self.board
+    
+    def display_board(self):
+        """
+        Displays user board in a clean and formatted structure.
+        """
+        board_str = f""
+        for row in range(board_size):
+            row_str = f"{self.board[row][0]} {self.board[row][1]} {self.board[row][2]} {self.board[row][3]} {self.board[row][4]}\n"
+            board_str += row_str
+        
+        return board_str
+
+
 
 user = Board(board_size, num_ships, user_name)
 computer = Board(board_size, num_ships, "computer")
 
-
-print(user.board)
-
-
+print(user.display_board())
